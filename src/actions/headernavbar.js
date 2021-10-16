67590aa2b460b663009e883daf4359bc948f6241
merypineda <meryp2@icloud.com>
2021-10-16 16:28:09 -0400

    finished navbar feature

--- src/actions/headernavbar.js
+++ src/actions/headernavbar.js
@@ -1,6 +1,7 @@
 import {
     SET_HEADER_LINKS,
-    SET_NAVBAR_LINKS
+    SET_NAVBAR_LINKS,
+    CHANGE_NAVBAR_ACTIVE
 } from './types';
 
 
@@ -17,3 +18,11 @@ export function setNavbarLinks(links) {
         payload: links
     })
 }
+
+export function changeNavbarActive(_id) {
+    return ({
+        type: CHANGE_NAVBAR_ACTIVE,
+        payload: _id
+    })
+}
+
--- src/reducers/headernavbarReducer.js
+++ src/reducers/headernavbarReducer.js
@@ -1,12 +1,13 @@
 import {
     SET_HEADER_LINKS,
-    SET_NAVBAR_LINKS
+    SET_NAVBAR_LINKS,
+    CHANGE_NAVBAR_ACTIVE
 } from '../actions/types';
 // the reducer component 
 
 const INITIAL_STATE = {
     headerLinks: [],
-    navbarLinks: []
+    navbarLinks: [],
 }
 
 export default function(state = INITIAL_STATE, action) {
@@ -22,6 +23,19 @@ export default function(state = INITIAL_STATE, action) {
                 ...state,
                 navbarLinks: action.payload
             }
+
+            case CHANGE_NAVBAR_ACTIVE:
+                const navbarLinks = state.navbarLinks.map(link => {
+                    link.active = false;
+                    if(link._id == action.payload) {
+                        link.active = true;
+                    }
+                    return link
+                })
+                return {
+                    ...state,
+                    navbarLinks
+                }
         default: return state;
     }
 }
