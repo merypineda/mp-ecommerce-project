af6c0171ed5f27b5a4a1143d45c4961f05174ae6
merypineda <meryp2@icloud.com>
2021-10-18 20:50:25 -0400

    built front of card and implemented rotation/flip animation ability

--- src/components/shop/shopProduct.js
+++ src/components/shop/shopProduct.js
@@ -2,25 +2,30 @@ import React, { Component } from 'react';
 import GreenPriceTag from '../greenPrice';
 import Quantity from '../quantity';
 
-class ShopProducts extends Component {
+class ShopProduct extends Component {
     render() {
         const { _id, title, description, price } = this.props // extracting
         return (
             <div className='shop-product'>
-                <div className='shop-product__title'>
-                    {title}
-                </div>
-                <div className='shop-product__description'>
-                        {description}
-                </div>
-                <GreenPriceTag className='shop-product__price' title={price} />
-              
-                <Quantity className='shop-product__quantity' quantity={1} />
-                <div className='shop-product__add-to-cart'>
-                    Add to Cart
-                </div>
-          </div>
+                <div className='shop-product__front'>
+                <img className='shop-product__front__image' src='http://via.placeholder.com/220x220'/>
+                    <div className='shop-product__front__title'>{title}</div>
+                 </div>
+                 <div className='shop-product__back'>
+                     <div className='shop-product__back__title'>
+                         {title}
+                     </div>
+                     <div className='shop-product__back__description'>
+                         {description}
+                     </div>
+                     <GreenPriceTag className='shop-product__back__price' title={price}/>
+                     <Quantity className='shop-product__back__quantity' quantity={1}/>
+                     <div className='shop-product__back__add-to-cart'>
+                         Add to Cart
+                     </div>
+                 </div>
+             </div>
         );
     }
 }
-export default ShopProducts;
+export default ShopProduct;
