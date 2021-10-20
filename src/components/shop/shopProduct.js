ce86fa65558a1b11254d4ac8c03ba50d239d2119
merypineda <meryp2@icloud.com>
2021-10-19 22:23:15 -0400

    built order review products component

--- src/components/shop/shopProduct.js
+++ src/components/shop/shopProduct.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import GreenPriceTag from '../greenPrice';
 import Quantity from '../quantity';
+import GreenPriceTag from '../greenPrice';
 
 import * as actions from '../../actions';
 import { connect } from 'react-redux';
@@ -10,37 +10,39 @@ class ShopProduct extends Component {
     handleAddToCart = () => {
         if(document.getElementById('shop-cart').classList.contains('cart-hidden')) {
             document.getElementById('shop-cart').classList.remove('cart-hidden');
-            const { _id, title, description, price, belongsTo } = this.props;
-            this.props.addCartProduct({ _id, title, description, price, belongsTo })
+            const { _id, title, description, price, belongsTo, imageUrl } = this.props;
+            this.props.addCartProduct({ _id, title, description, price, belongsTo, imageUrl });
         } else {
             document.getElementById('shop-cart').classList.add('cart-hidden');
         }
     }
+
     render() {
-        const { _id, title, description, price } = this.props // extracting
+        const { _id, title, description, price } = this.props;
         return (
             <div className='shop-product'>
                 <div className='shop-product__front'>
-                <img className='shop-product__front__image' src='http://via.placeholder.com/220x220'/>
+                    <img className='shop-product__front__image' src='http://via.placeholder.com/220x220'/>
                     <div className='shop-product__front__title'>{title}</div>
-                 </div>
-                 <div className='shop-product__back'>
-                     <div className='shop-product__back__title'>
-                         {title}
-                     </div>
-                     <div className='shop-product__back__description'>
-                         {description}
-                     </div>
-                     <GreenPriceTag className='shop-product__back__price' title={price}/>
-                     <Quantity className='shop-product__back__quantity' quantity={1}/>
-                     <a onClick={this.handleAddToCart} className='shop-product__back__add-to-cart'>
-                         Add to Cart
-                     </a>
-                 </div>
-             </div>
-        );
+                </div>
+                <div className='shop-product__back'>
+                    <div className='shop-product__back__title'>
+                        {title}
+                    </div>
+                    <div className='shop-product__back__description'>
+                        {description}
+                    </div>
+                    <GreenPriceTag className='shop-product__back__price' title={price}/>
+                    <Quantity className='shop-product__back__quantity' quantity={1}/>
+                    <a onClick={this.handleAddToCart} className='shop-product__back__add-to-cart'>
+                        Add to Cart
+                    </a>
+                </div>
+            </div>
+        )
     }
 }
-ShopProduct = connect(null, actions)(ShopProduct)
+
+ShopProduct = connect(null, actions)(ShopProduct);
 
 export default ShopProduct;
