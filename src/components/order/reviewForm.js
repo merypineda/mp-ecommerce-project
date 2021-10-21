9b5e59bd1129decce7898e563f3f7790453c0241
merypineda <meryp2@icloud.com>
2021-10-20 20:00:27 -0400

    order review products title bar

--- src/components/order/reviewForm.js
+++ src/components/order/reviewForm.js
@@ -14,25 +14,29 @@ class ReviewForm extends Component {
 
         return (
             <form onSubmit={handleSubmit} className={`${className} sign-up-form`}>
-            <ReviewProducts className='review-form__products'/>
-            <div className='review-form__line'></div>
-            <Field className='review-form__proceed'
-            onClick={() => history.push('/signin')}
-            type='submit'
-            title='Proceed to Checkout'
-            name='proceed'
-            component={FormButton} />
+                <div className='review-form__legend'>
+        <div className='review-form__legend__name'>Name</div>
+                    <div className='review-form__legend__quantity'>Quantity</div>
+                    <div className='review-form__legend__price'>Price</div>
+                </div>
+                <ReviewProducts className='review-form__products'/>
+                <div className='review-form__line'></div>
+                <Field className='review-form__proceed'
+                onClick={() => history.push('/signin')}
+                type='submit'
+                title='Proceed to Checkout'
+                name='proceed'
+                component={FormButton} />
 
-            <Field className='review-form__back'
-            onClick={() => history.push('/shop')}
-            type='button'
-            title='Back'
-            name='back'
-            short={true}
-            component={FormButton} />
+                <Field className='review-form__back'
+                onClick={() => history.push('/shop')}
+                type='button'
+                title='Back'
+                name='back'
+                short={true}
+                component={FormButton} />
 
-
-        </form>
+            </form>
         )
     }
 }
